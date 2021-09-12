64e8366233333e8f53fdd925388e376c806b56cb
Ishan Bhatt <ivbhatt@outlook.com>
2021-09-12 11:43:32 -0400

    remove jQuery, read data from js files

---
+++ quotes.js
@@ -0,0 +1,6 @@
+quotes_corpus=`[
+{"quote": "Ullamco labore laborum ad irure velit ut sint in cillum labore sit in ad et sit aute dolor pariatur reprehenderit.", "author" : "sb sb"},
+{"quote": "Yirure velit ut sint in cillum labore sit in ad et sit aute dolor pariatur reprehenderit.", "author" : "sb qw"},
+{"quote": "Labore laborum ad irure velit ut sint in cillum labore sit in ad et sit aute dolor pariatur reprehenderit.", "author" : "sb gvd"},
+{"quote": "Ad irure velit ut sint in cillum labore sit in ad et sit aute dolor pariatur reprehenderit.", "author" : "dfsd sb"}
+]`;
