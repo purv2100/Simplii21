4e01a8f1cc71e70d2800be6df23c102373f460c3
Ishan Bhatt <ivbhatt@outlook.com>
2021-09-23 21:02:13 -0400

    ishan tasks-1

--- app.py
+++ app.py
@@ -1,32 +1,97 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+
 
 import os, sys
 import random
 import csv
+import json
 
 app = Flask(__name__)
 
 
+
+ALL_QUOTES = []
+ALL_AUTHORS = []
+with open(os.path.join("static", "quotes_dataset.csv"), "r") as csv_file:
+  reader = csv.DictReader(csv_file)
+
+  for row in reader:
+    ALL_QUOTES.append(row["quote"])
+    ALL_AUTHORS.append(row["author"])
+
+
+
+def refresh_data():
+
+
+  with open(os.path.join("static", "user_information.json"), "r") as json_file:
+    json_data = json.load(json_file)
+
+  initialized = json_data["initialized"]
+  name = json_data["name"]
+  email_id = json_data["email_id"]
+  email_notifications = json_data["email_notifications"]
+  
+
+  index = random.randint(0, len(ALL_AUTHORS)-1)
+
+  data = {
+    "name_block" : {"initialized":initialized, "name": name, "email_id": email_id,
+     "email_notifications": email_notifications},
+    "quote_block" : {"quote": ALL_QUOTES[index], "author": ALL_AUTHORS[index]}
+  }
+
+  return data
+
+
+def delete_tasks():
+  pass
+
+
+def delete_user_information():
+  empty_user_information = {
+    "initialized" : "no",
+    "name" : "",
+    "email_id" : "",
+    "email_notifications" : "no"
+  }
+
+  with open(os.path.join("static", "user_information.json"), "w") as json_file:
+    json.dump(empty_user_information, json_file)
+
+
+
 @app.route("/")
 def homePage():
-
-  all_quotes = []
-  all_authors = []
-
-
-  with open(os.path.join("static", "quotes.csv")) as csv_file:
-    reader = csv.DictReader(csv_file)
-
-    for row in reader:
-      all_quotes.append(row["quote"])
-      all_authors.append(row["author"])
-
-
-  index = random.randint(0, len(all_authors)-1)
-
-
-  return render_template("index.html", quote= all_quotes[index], author = all_authors[index])
+  return render_template("index.html", data=refresh_data())
   
   
+@app.route("/update_user_info", methods = ["POST"])
+def update_user_information():
+  
+  user_information = json.loads(request.data)
+  
+  with open(os.path.join("static", "user_information.json"), "w") as json_file:
+    json.dump(user_information, json_file)
+
+  return render_template("index.html", data=refresh_data())
+
+
+
+@app.route("/reset_all", methods = ["POST"])
+def delete_user():
+  delete_tasks()
+  delete_user_information()
+
+  return render_template("index.html", data=refresh_data())
+
+
+@app.route("/reset_tasks", methods = ["POST"])
+def delete_tasks_only():
+  delete_tasks()
+
+  return render_template("index.html", data=refresh_data())
+
 app.run(debug = True)
