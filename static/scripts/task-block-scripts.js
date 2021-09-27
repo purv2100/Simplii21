ada26ec2e57a97bf404eb070ec94356247f5b54c
Krishika Shivnani <krishikashivnani@Krishikas-MacBook-Pro.local>
2021-09-26 20:42:52 -0400

    Merged conflict

--- static/scripts/task-block-scripts.js
+++ static/scripts/task-block-scripts.js
@@ -3,11 +3,27 @@ function showVal(newVal){
 document.getElementById("valBox").innerHTML=newVal;
 }
 
+function show1Val(newVal){
+    document.getElementById("val1Box").innerHTML=newVal;
+    }
+
+    function show2Val(newVal){
+        document.getElementById("val2Box").innerHTML=newVal;
+        }
+
 // Code to set up current time on deadline datefield. 
 window.addEventListener('load', () => {
 const now = new Date();
 now.setMinutes(now.getMinutes() - now.getTimezoneOffset());
 document.getElementById('deadline').value = now.toISOString().slice(0, -8);
+});
+
+$(document).ready(function(){
+    $('input[type="radio"]').click(function(){
+        var inputValue = $(this).attr("value");
+        $("div.desc").hide();
+        $("#" + inputValue + "Split").show();
+    });
 });
 
 function submitTask(formData) {
