5624488eb49ab5c9080502730272a5ad91afee50
Unnati Nadupalli <prema.unnati@gmail.com>
2021-09-30 22:52:57 -0400

    added all test cases

--- test.py
+++ test.py
@@ -6,9 +6,6 @@ import requests
 
 class TestModule(unittest.TestCase):
 
-
-
-
     def test_home(self):
         tester = app.test_client(self)
         response = tester.get("/", content_type='html/text')
@@ -20,12 +17,17 @@ class TestModule(unittest.TestCase):
         response = requests.post("http://127.0.0.1:5000/add_task", data=inp)
         self.assertEqual(response.status_code, 200)
 
+    def test_delete_task(self):
+        tester = app.test_client(self)
+        inp = {"id" : "23"}
+        response = requests.post("http://127.0.0.1:5000/delete_task", data=inp)
+        self.assertEqual(response.status_code, 200)
+
     def test_update_user_info(self):
         tester = app.test_client(self)
         inp = {"name" : "user1", "email":"user1@gmail.com","emailChoose":"user1@gmail.com"}
         response = requests.post("http://127.0.0.1:5000/update_user_info", data=inp)
         self.assertEqual(response.status_code, 200)
-
 
 if __name__ == "__main__":
      unittest.main()
