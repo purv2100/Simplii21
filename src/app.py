1f37d19108ae8c4a5ca1893d40819a8e636faded
radhikaraman20 <89432698+radhikaraman20@users.noreply.github.com>
2021-11-03 15:46:02 -0400

    DB integration for signup page, inclusion of user ID in login page

--- src/app.py
+++ src/app.py
@@ -14,6 +14,7 @@ from pymongo import MongoClient
 from flask import Flask
 from flask import render_template, url_for
 from flask import request, redirect
+from flask import flash
 
 #MongoDB connection using cluster's connection string
 client = pymongo.MongoClient("mongodb+srv://radhika:Radhika1997@simplii.tvhh1.mongodb.net/simplii?retryWrites=true&w=majority")
@@ -24,6 +25,7 @@ testUserInfo = db.testUserInfo
 
 
 app = Flask(__name__, static_folder='static')
+app.secret_key = "simpliitesting"
 package_dir = os.path.dirname(os.path.abspath(__file__))
 """Global constant to store directory path""" 
 TODO_TASKS_PATH = os.path.join(package_dir, "../static", "tasks", "todo")
@@ -93,13 +95,17 @@ def getnewTaskID():
 
 
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def homePage():
     """This function renders the landing page."""
     return render_template("base.html")
 
-@app.route("/login", methods=['GET', 'POST'])
-def login():
+@app.route("/login", methods=['GET'])
+def login_get():
+    return render_template('login.html')
+
+@app.route("/login", methods=['POST'])
+def login_post():
     error = None
     if request.method == 'POST':
         if request.form['email'] != 'admin@gmail.com' or request.form['password'] != 'admin':
@@ -108,7 +114,7 @@ def login():
             return redirect("/index")
     return render_template('login.html')
 
-
+'''
 @app.route("/signup", methods=['GET','POST'])
 def signup():
     error = None
@@ -128,53 +134,59 @@ def signup():
         #session['username'] = reg_username
         return redirect(url_for('login'))
     return render_template('signup.html')
-
 '''
-@app.route("/signup", methods=['GET', 'POST'])
-def signup():
+
+@app.route("/signup", methods=['GET'])
+def signup_get():
+    return render_template('signup.html')
+
+@app.route("/signup", methods=['POST'])
+def signup_post():
     message = ''
 
-    
+    '''
     if "email" in session:
         return redirect(url_for("logged_in"))
-
+        
+    '''
 
     if request.method == 'POST':
-        #user_id = request.form.get("userid")
-        #user_first_name = request.form.get("firstname")
-        user_name = request.form.get("name")
-        #user_last_name = request.form.get("lastname")
+        user_id = request.form.get("userid")
+        user_first_name = request.form.get("firstname")
+        user_last_name = request.form.get("lastname")
         email = request.form.get("email")
         user_password = request.form.get("password")
 
-        user_found = testUserInfo.find_one({"first_name": user_name})
+        user_found = testUserInfo.find_one({"user_id": user_id})
         email_found = testUserInfo.find_one({"email_id": email})
 
 
         if user_found:
-            message = 'User ID already taken, please enter a different user ID'
-            return render_template('signup.html', message=message)
+            flash('User ID already taken, please enter a different user ID')
+            #message = 'User ID already taken, please enter a different user ID'
+
+            return redirect(url_for('signup_post'))
+
+            #return render_template('signup.html', message=message)
 
 
         if email_found:
             message = 'This email already exists in database'
             return render_template('signup.html', message=message)
 
-
         else:
             pw_hashAndSalt = bcrypt.hashpw(user_password.encode(), bcrypt.gensalt())
 
-            #user_input_data = {'user_id':user_id, 'first_name': user_first_name, 'last_name': user_last_name, 'email_id': email, 'password': pw_hashAndSalt}
-            user_input_data = {'first_name': user_name, 'email_id': email, 'password': pw_hashAndSalt}
+            user_input_data = {'user_id': user_id, 'first_name': user_first_name, 'last_name': user_last_name, 'email_id': email, 'password': pw_hashAndSalt}
+            #user_input_data = {'first_name': user_name, 'email_id': email, 'password': pw_hashAndSalt}
 
             testUserInfo.insert_one(user_input_data)
 
             user_data = testUserInfo.find_one({"email_id": email})
             new_email = user_data['email_id']
 
-            return render_template('index.html', email=new_email)
+            return redirect(url_for('mainPage'))
     return render_template('index.html')
-'''
 
 @app.route("/index")
 def mainPage():
