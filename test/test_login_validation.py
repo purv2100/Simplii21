321f1a20d4d9152f8d3fac4feaeb23b9792e6ac1
Sunidhi Hegde <62884191+SunidhiHegde@users.noreply.github.com>
2021-11-04 17:34:35 -0400

    Update test_login_validation.py

--- test/test_login_validation.py
+++ test/test_login_validation.py
@@ -1,2 +1,18 @@
-inp = {"name": "user1", "email": "user1@gmail.com", "emailChoose": "user1@gmail.com"}
-        response = requests.post("http://127.0.0.1:5000/update_user_info", data=inp)
+import unittest
+from src.app import app
+import requests
+
+
+app.testing = True
+
+class TestModule(unittest.TestCase):
+        
+    def test_login(self):
+        tester = app.test_client(self)
+        inp = {"userid": "sriram123", "password": "sriram12345"}
+        response = requests.post("http://127.0.0.1:5000/login", data=inp)
+        self.assertEqual(response.status_code, 200)
+        
+        
+if __name__ == "__main__":
+    unittest.main()
