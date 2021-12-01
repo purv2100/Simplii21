5549e1c6289e68521eb568c9f84dbc2dbb54a99b
AtharvaJ10 <atharvajoshi10@gmail.com>
2021-12-01 18:08:03 -0500

    Adding test cases for EditTask

--- src/test/tests.py
+++ src/test/tests.py
@@ -6,7 +6,17 @@ class BasicTestCase(unittest.TestCase):
     def test_task(self):
         self.app=application.app.test_client()
         ans=self.app.get('/task')
+        self.assertEqual(ans.status_code,302)
+
+    def test_editTask(self):
+        self.app=application.app.test_client()
+        ans=self.app.get('/editTask')
         self.assertEqual(ans.status_code,200)
+    
+    def test_updateTask(self):
+            self.app=application.app.test_client()
+            ans=self.app.get('/updateTask')
+            self.assertEqual(ans.status_code,302)
 
 if __name__ == '__main__':
     unittest.main()
