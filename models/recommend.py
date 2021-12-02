344d259cbcac15fbb5794e67a7a7d60badff19dc
shivam-pednekar <shivampednekar10@gmail.com>
2021-12-02 02:59:49 -0500

    task recommendations

---
+++ models/recommend.py
@@ -0,0 +1,90 @@
+import pymongo
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import datetime
+import os
+import sys
+from datetime import date
+
+client = pymongo.MongoClient('mongodb://localhost:27017')
+db = client["simplii"]
+p_details = db["tasks"]   #profile details
+records = p_details.find()
+list_record = list(records)
+
+df = pd.DataFrame(list_record)
+
+start = df['startdate'].tolist()
+due = df['duedate'].tolist()
+
+format = '%Y-%m-%d'
+
+st, dt =[],[]
+for i in start:
+    st.append(datetime.datetime.strptime(i, format).date())
+
+for i in due:
+    dt.append(datetime.datetime.strptime(i, format).date())
+
+in_prog = df.loc[df['status'] == 'In Progress']
+in_prog = in_prog.drop(['email', '_id', 'status'], axis=1)
+
+blocked = df.loc[df['status'] == 'Blocked']
+blocked =blocked.drop(['email', '_id', 'status'], axis=1)
+
+sip,dip,sb,db =[],[],[],[]
+start1 = in_prog['startdate'].tolist()
+due1 = in_prog['duedate'].tolist()
+
+start2 = blocked['startdate'].tolist()
+due2 = blocked['duedate'].tolist()
+
+for i in start1:
+    sip.append(datetime.datetime.strptime(i, format).date())
+
+for i in start2:
+    sb.append(datetime.datetime.strptime(i, format).date())
+
+for i in due1:
+    dip.append(datetime.datetime.strptime(i, format).date())
+
+for i in due2:
+    db.append(datetime.datetime.strptime(i, format).date())
+
+in_prog['startdate']= pd.to_datetime(in_prog['startdate'])
+in_prog['duedate']= pd.to_datetime(in_prog['duedate'])
+
+blocked['startdate']= pd.to_datetime(blocked['startdate'])
+blocked['duedate']= pd.to_datetime(blocked['duedate'])
+
+
+df1 = in_prog.loc[(in_prog['startdate'] <= pd.to_datetime('today').floor('D')) & (in_prog['duedate'] >= pd.to_datetime('today').floor('D'))]
+df2 = blocked.loc[(blocked['duedate'] >= pd.to_datetime('today').floor('D'))]
+df1 =blocked.drop(['startdate'], axis=1)
+df2 =blocked.drop(['startdate'], axis=1)
+
+
+#(blocked['startdate'] <= pd.to_datetime('today').floor('D')) & 
+
+#RECOMMENDATIONS IN TXT FILE
+with open(os.path.join(sys.path[0], "task_recommendation.txt"), 'wt') as f:
+    x = df1.to_csv(sep='\t', index=False, header=True)
+    y = df2.to_csv(sep='\t', index=False, header=True)
+
+    f.write("Here's your task recommendation on tasks you're working...\n\n")
+    f.write(x)
+    f.write("\n\n")
+    f.write("Here are some task recommnedation so you don't miss the deadlines...\n\n")
+    f.write(y)
+
+#RECOMMENDATIONS IN CSV FILE
+with open(os.path.join(sys.path[0], "task_recommendation.csv"), 'wt') as f:
+    x = df1.to_csv(index=False, header=True)
+    y = df2.to_csv(index=False, header=True)
+
+    f.write("Here's your task recommendation on tasks you're working...\n\n")
+    f.write(x)
+    f.write("\n\n")
+    f.write("Here are some task recommnedation so you don't miss the deadlines...\n\n")
+    f.write(y)
