76ada843c502cc8e28bf48e21d5098451aed717c
kskheni <kkheni11@gmail.com>
2023-10-17 18:23:07 -0400

    fix: rename tests.py file to comply pytests convensions

--- lint.sh
+++ lint.sh
@@ -3,8 +3,8 @@ autopep8 --in-place --aggressive src/application.py
 echo "src/application.py complete"
 autopep8 --in-place --aggressive src/apps.py
 echo "src/apps.py complete"
-autopep8 --in-place --aggressive src/test/tests.py
-echo "src/test/tests.py complete"
+autopep8 --in-place --aggressive src/test/unit_test.py
+echo "src/test/unit_test.py complete"
 autopep8 --in-place --aggressive src/forms.py
 echo "src/forms.py complete"
 autopep8 --in-place --aggressive models/recommend.py
