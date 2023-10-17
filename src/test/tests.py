c45a618ece1263072981d55599cc9c8e73ae24be
kskheni <kkheni11@gmail.com>
2023-10-17 17:08:34 -0400

    fix: solve import error due to changes in folder structure

--- src/test/tests.py
+++ src/test/tests.py
@@ -1,66 +1,68 @@
 import unittest
-import application
-
+import sys
+import os
+sys.path.insert(0, os.path.join(os.getcwd(), 'src'))
+from application import app
 
 class BasicTestCase(unittest.TestCase):
 
     def test_login(self):
-        self.app = application.app.test_client()
+        self.app = app.test_client()
         ans = self.app.get('/login')
         self.assertEqual(ans.status_code, 200)
 
     def test_home(self):
-        self.app = application.app.test_client()
+        self.app = app.test_client()
         ans = self.app.get('/home')
         self.assertEqual(ans.status_code, 302)
 
     def test_forgotPassword(self):
-        self.app = application.app.test_client()
+        self.app = app.test_client()
         ans = self.app.get('/forgotPassword')
-        self.assertEqual(ans.status_code, 302)
+        self.assertEqual(ans.status_code, 200)
 
     def test_dashboard(self):
-        self.app = application.app.test_client()
+        self.app = app.test_client()
         ans = self.app.get('/dashboard')
         self.assertEqual(ans.status_code, 200)
 
     def test_about(self):
-        self.app = application.app.test_client()
+        self.app = app.test_client()
         ans = self.app.get('/about')
         self.assertEqual(ans.status_code, 200)
 
     def test_register(self):
-        self.app = application.app.test_client()
+        self.app = app.test_client()
         ans = self.app.get('/register')
         self.assertEqual(ans.status_code, 200)
 
     def test_task(self):
-        self.app = application.app.test_client()
+        self.app = app.test_client()
         ans = self.app.get('/task')
         self.assertEqual(ans.status_code, 302)
 
     def test_editTask(self):
-        self.app = application.app.test_client()
+        self.app = app.test_client()
         ans = self.app.get('/editTask')
         self.assertEqual(ans.status_code, 200)
 
     def test_updateTask(self):
-        self.app = application.app.test_client()
+        self.app = app.test_client()
         ans = self.app.get('/updateTask')
         self.assertEqual(ans.status_code, 302)
 
     def test_logout(self):
-        self.app = application.app.test_client()
+        self.app = app.test_client()
         ans = self.app.get('/logout')
         self.assertEqual(ans.status_code, 200)
 
     def test_dummy(self):
-        self.app = application.app.test_client()
+        self.app = app.test_client()
         ans = self.app.get('/dummy')
         self.assertEqual(ans.status_code, 200)
 
     def test_delete(self):
-        self.app = application.app.test_client()
+        self.app = app.test_client()
         ans = self.app.get('/deleteTask')
         self.assertEqual(ans.status_code, 200)
 
