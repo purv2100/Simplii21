3271d7fc9b5b6207616c93481b8393dd27309880
surya-sukumar <79688972+surya-sukumar@users.noreply.github.com>
2023-10-18 09:47:11 -0400

    change

--- src/test/unit_test.py
+++ src/test/unit_test.py
@@ -1,7 +1,7 @@
 import unittest
 import sys
 import os
-sys.path.insert(0, os.path.join(os.getcwd(), '../src'))
+sys.path.insert(0, os.path.join(os.getcwd(), '/../src'))
 from application import app
 
 class BasicTestCase(unittest.TestCase):
