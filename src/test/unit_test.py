9943cee57c1cc12ff4841baaa97bf7861ff2928d
surya-sukumar <79688972+surya-sukumar@users.noreply.github.com>
2023-10-18 16:32:43 -0400

    Fix test

--- src/test/unit_test.py
+++ src/test/unit_test.py
@@ -1,7 +1,10 @@
+import inspect
 import unittest
 import sys
 import os
-currentdir - os.path.dirname(os .path. abspath(inspect -getfile(inspect .current frame ()))) parentdir = os.path. dirname (currentdir) sys-path.insert(0, parentdir)
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir) 
+sys.path.insert(0, parentdir)
 from application import app
 
 class BasicTestCase(unittest.TestCase):
